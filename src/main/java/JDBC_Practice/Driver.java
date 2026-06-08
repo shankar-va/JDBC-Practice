fa584939909bfb1a89cb366b90a1ac0933dae1e5
SHANKAR <shankarfrom2004@gmail.com>
2026-06-08 15:27:56 +0530

    connect jdbc with postgre sql

--- src/main/java/JDBC_Practice/Driver.java
+++ src/main/java/JDBC_Practice/Driver.java
@@ -1,5 +1,71 @@
 package JDBC_Practice;
 
-public class Driver {
+import java.util.Scanner;
 
+public class Driver {
+	 
+	public static void main(String[] args) {
+		System.out.println("This is sample JDBC connectivity program");
+		Scanner  scn =new  Scanner(System.in);
+		while(true) {
+			System.out.println("Enter options to perfrom CRUD operations : ");
+			System.out.println("1)SELECT    2)INSERT    3)UPDATE    4)DELETE    5)CLOSE");
+			int option=scn.nextInt();
+			String name="";
+			int age=0;
+			int oldId=0;
+			int newId=0;
+			if(option==2) {
+				System.out.println("Enter Student details to add : ");
+				System.out.println("Enter Student name");
+				scn.nextLine();
+				name=scn.nextLine();
+				System.out.println("Enter Student age : ");
+				age=scn.nextInt();
+			}else if(option==3) {
+				System.out.println("Enter the id of Student to be updated : ");
+				oldId=scn.nextInt();
+				System.out.println("Enter new id of the Student : ");
+				newId=scn.nextInt();
+				System.out.println("Enter new name of the Student :");
+				scn.nextLine();
+				name=scn.nextLine();
+				System.out.println("Enter new age of the  Student : ");
+				age=scn.nextInt();
+			}
+			switch(option) {
+			case 1:
+				Service.selectAll();
+				break;
+			case 2:
+				int id=Service.getMaxId();
+				if(Service.add(new Student(id++, name, age))==1) {
+					System.out.println("Student added successfully");
+					
+				}
+				else System.out.println("Student cannot be added");
+				break;
+			case 3:
+				if(Service.updateById(new Student(newId, name, age), oldId)==1) 
+					System.out.println("Student updated successfully");
+				else System.out.println("Student cannot be updated");
+				break;
+			case 4:
+				System.out.println("Enter Student Id to be deleted : ");
+				int deleteId=scn.nextInt();
+				if(Service.removeById(deleteId)==1)
+					System.out.println("Student updated successfully");
+				else System.out.println("Student cannot be updated");
+				break;
+			case 5:
+				if(Service.closeConnection())
+					System.out.println("Thank you!!! Welcome ");
+				else System.out.println("Database connection cannot be closed");
+				System.exit(1);
+				break;
+			default:
+				System.out.println("Invalid Input!!!");
+			}
+		}
+	}
 }
--- src/main/java/JDBC_Practice/Service.java
+++ src/main/java/JDBC_Practice/Service.java
@@ -1,5 +1,112 @@
 package JDBC_Practice;
+import java.security.interfaces.RSAKey;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Service {
-
+	static Connection connection;
+	private static String user="postgres";
+	private static String pswd="123";
+	private static String url="jdbc:postgresql://localhost:5432/postgres";
+	private static PreparedStatement pstm;
+	static {
+		try {
+			Class.forName("org.postgresql.Driver");
+			connection=DriverManager.getConnection(url,user,pswd);
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	public static int add(Student s) {
+		String query="INSERT into student values(?,?,?);";
+		
+		int res=0;
+		try {
+			pstm = connection.prepareStatement(query);
+			pstm.setInt(1,s.getId());
+			pstm.setString(2, s.getName());
+			pstm.setInt(3, s.getAge());
+			res=pstm.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return res;
+	}
+	public static int removeById(int id) {
+		String query = "DELETE FROM student where id=?";
+		int res=0;
+		try {
+			pstm=connection.prepareStatement(query);
+			pstm.setInt(1, id);
+			res=pstm.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return res;
+	}
+	public static int updateById(Student s,int id) {
+		String query="UPDATE student SET id=?,name=?,age=? where id=?";
+		int res=0;
+		try {
+			pstm=connection.prepareStatement(query);
+			pstm.setInt(1, s.getId());
+			pstm.setString(2, s.getName());
+			pstm.setInt(3, s.getAge());
+			pstm.setInt(4, id);
+			res=pstm.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return res;
+	}
+	public static int getMaxId() {
+		String query="SELECT MAX(id) FROM student;";
+		try {
+			pstm=connection.prepareStatement(query);
+			ResultSet res=pstm.executeQuery();
+			if(res.next())return res.getInt(1)+1;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	public static void selectAll() {
+		String query="SELECT * FROM student";
+		try {
+			pstm=connection.prepareStatement(query);
+			ResultSet res=pstm.executeQuery();
+			System.out.println("Id\tName\tAge");
+			while(res.next()) {
+				System.out.println(res.getInt(1)+"\t"+res.getString(2)+"\t"+res.getInt(3));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static boolean closeConnection() {
+		try {
+			connection.close();
+			return true;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
 }
